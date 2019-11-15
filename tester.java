15ddf3f62ff9fcc8009fc977644f8b3a9945a1dd
Harry Singh <harpreetgoraya95@gmail.com>
2019-11-14 21:16:44 -0500

    added tester class and updb method

---
+++ tester.java
@@ -0,0 +1,14 @@
+
+public class test{
+
+
+public static void main(String[] args) {
+
+    Assignment2 a2 = new Assignment2();
+    a2.myDriver = "a2";
+
+    a2.insertPlayer(55, "Goraya", 28, 1);
+    
+    }
+
+}
